f9987fbe464100f51a6ddb44b916653d46a8a134
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-04-18 20:51:29 +0300

    Updating files

--- Context-with-useReduce/src/pages/Home.jsx
+++ Context-with-useReduce/src/pages/Home.jsx
@@ -3,17 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    {/* <h1 className='bg-amber-950  text-white font-black p-20 m-4 ' >Home page</h1> */}
-    <main className='w-full'  >
-      <section>
-        <article>
-          <img src="image" alt="Grid_Image" />
-          <h2>Card Tittle</h2>
-          <p>Card Paragraph</p>
-          <button type="submit">Read..</button>
-        </article>
-      </section>
-    </main>
+    
     </>
   )
 }
