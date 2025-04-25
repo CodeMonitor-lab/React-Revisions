be82a939508d4efba462db5f8921683c9b7a5c65
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-04-25 17:11:00 +0300

    Code-Fixes

--- React-Cards-Layout/src/components/CardDetail.jsx
+++ React-Cards-Layout/src/components/CardDetail.jsx
@@ -21,7 +21,7 @@ const CardDetail = () => {
     <main className='bg-sky-200 min-w-screen min-h-screen'>
       <section className='bg-blue-500 min-h-screen p-5 flex  items-center justify-center ' >
         <article className='p-5 bg-red-400 ' >
-          <p className='mb-5 font-black' > Post Id : {post.id}</p>
+          <p className='mb-5 font-black bg-center bg-cover ' > Post Id : {post.id}</p>
         <img className='w-full  h-80 '  src={post.img} alt="Content-image" loading='lazy' />
         <h1 className='font-bold text-3xl mt-5 text-black' >{post.tittle}</h1>
         <p className='text-2xl font-mono pt-2 pb-5' >{post.Content}</p>
